cc27bc412663013bc29dceee6db403b6fe23b309
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-20 12:19:55 +0100

    exo 2

--- exo 2/script.js
+++ exo 2/script.js
@@ -1,7 +1,22 @@
-const showButton = document.querySelector('#showButton');
-const hiddenBox = document.querySelector('#hiddenBox');
-const body = document.querySelector('body'); 
 
-showButton.addEventListener('click', handleShowButton){
-    hiddenBox.style.display = 'block';   
- 
+const showButton = document.querySelector('#showButton');
+const body = document.querySelector('body'); 
+const hiddenBox = document.querySelector('#hiddenBox');
+
+showButton.addEventListener('click' , handleClick);
+body.addEventListener('click' , handleClickOut);
+
+
+function handleClick(){
+
+hiddenBox.classList.remove('hidden');
+body.classList.add('grey-background');
+}
+
+function handleClickOut(event) {
+    
+    if (event.target !== hiddenBox && event.target !== showButton) {
+      hiddenBox.classList.add('hidden');
+      body.classList.remove('grey-background');
+    }
+  }
