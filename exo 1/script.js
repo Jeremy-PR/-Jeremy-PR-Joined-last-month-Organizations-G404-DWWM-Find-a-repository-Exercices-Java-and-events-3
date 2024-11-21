ee59ddcca9d3b18af9c69b7280a9f92f60296659
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-21 14:35:28 +0100

    exo correction

--- exo 1/script.js
+++ exo 1/script.js
@@ -1,20 +1,49 @@
-const cases = document.querySelectorAll('.case');
-const colors = ['black', 'yellow' , 'blue', 'red','purple']
+// const cases = document.querySelectorAll('.case');
+// const colors = ['black', 'yellow' , 'blue', 'red','purple']
 
-cases.forEach((carre) => {
-    carre.addEventListener('click' , handleClickChangeColor)
-})
+// cases.forEach((carre) => {
+//     carre.addEventListener('click' , handleClickChangeColor)
+// })
 
-function handleClickChangeColor (event) {
+// function handleClickChangeColor (event) {
     
-    let bloc = event.target;
-    let ColorIndex = colors.indexOf(bloc.style.backgroundColor);
+//     let bloc = event.target;
+//     let ColorIndex = colors.indexOf(bloc.style.backgroundColor);
   
 
-    const nextColorIndex = (ColorIndex + 1) % colors.length;
+//     const nextColorIndex = (ColorIndex + 1) % colors.length;
 
-    bloc.style.backgroundColor = colors[nextColorIndex];
-
-
+//     bloc.style.backgroundColor = colors[nextColorIndex];
  
+// }
+
+
+// correction
+
+const boxes = document.querySelectorAll(".box");
+
+const colors = [
+  "bgcolor-green",
+  "bgcolor-blue",
+  "bgcolor-red",
+  "bgcolor-yellow",
+  "bgcolor-orange",
+  "bgcolor-purple",
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", handleClickChangeColor);
+});
+
+function handleClickChangeColor(event) {
+  const box = event.target;
+  const currentColorClass = colors.find(color => box.classList.contains(color));
+  const currentColorIndex = colors.indexOf(currentColorClass);
+  const nextColorIndex = (currentColorIndex + 1) % colors.length;
+  
+  const nextColorClass = colors[nextColorIndex];
+
+  box.classList.remove(currentColorClass);
+  box.classList.add(nextColorClass);
+  
 }
